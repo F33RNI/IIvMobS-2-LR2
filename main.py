1e21d1a7ffba0385c80bdeadf3b9c2ada537b8a7
F33RNI <xxoinvizionxx@gmail.com>
2024-02-13 15:50:52 -0500

    feat: edit code and docs for this branch

--- main.py
+++ main.py
@@ -25,200 +25,107 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <https://unlicense.org>
 """
 
-import logging
 import os
-import sys
-from typing import List, Tuple
 
 import cv2
 import numpy as np
 
-# Path to input image, video file or integer (0..) to use webcam instead
-PHOTO_VIDEO_INPUT = "videos/planes_stock.mp4"
-
-# Path to yolo directory with yolov4-tiny.cfg and yolov4-tiny.cfg files
-YOLO_DIR = "yolov4-tiny"
-
-# Files with YOLO class names inside YOLO_DIR
-YOLO_CLASSES_FILE = "coco.names.txt"
-
-# YOLO config file inside YOLO_DIR
-YOLO_CFG_FILE = "yolov4-tiny.cfg"
-
-# YOLO model weights file inside YOLO_DIR
-YOLO_WEIGHTS_FILE = "yolov4-tiny.weights"
+# Видеофайл / изображение или индекс вебки (TO_DETECT = 0)
+TO_DETECT = "photos/books_and_apples.jpg"
 
 
-def logging_setup() -> None:
-    """Sets up logging format and level"""
+def detect(frame, coco_net, output_layers):
+    # Размер входной картинки
+    height, width, _ = frame.shape
 
-    # Create logs formatter
-    log_formatter = logging.Formatter(
-        "%(asctime)s %(threadName)s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
-    )
-
-    # Setup logging into console
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(log_formatter)
-
-    # Add all handlers and setup level
-    root_logger = logging.getLogger()
-    root_logger.addHandler(console_handler)
-    root_logger.setLevel(logging.INFO)
-
-    # Log test message
-    logging.info("Logging setup is complete")
-
-
-def yolo_detect_and_localize(
-    image_to_process: np.ndarray,
-    net: cv2.dnn.Net,
-    out_layers: List[str],
-    classes: List[str],
-    colors: List[Tuple[int, int, int]],
-) -> np.ndarray:
-    """Recognizes and localizes object on image
-
-    Args:
-        image_to_process (np.ndarray): source image
-        net (cv2.dnn.Net): YOLO net object
-        out_layers (List[str]): YOLO output layers list
-        classes (List[str]): list of YOLO classes
-        colors (List[Tuple(int, int, int)]): list of colors of each class
-
-    Returns:
-        np.ndarray: image with annotations
-    """
-
-    height, width, _ = image_to_process.shape
-
-    # Prepare image
-    blob = cv2.dnn.blobFromImage(image_to_process, 1 / 255, (608, 608), (0, 0, 0), swapRB=True, crop=False)
-
-    # Feed image
-    net.setInput(blob)
-    outs = net.forward(out_layers)
-
-    # Parse results into class_indexes, class_scores and bounding boxes
+    # Подготовка списков для сохранения распознанных объектов
     class_indexes, class_scores, boxes = ([] for _ in range(3))
-    for out in outs:
+
+    # Вход нейросети
+    coco_net.setInput(cv2.dnn.blobFromImage(frame, 1 / 255, (608, 608), (0, 0, 0), swapRB=True, crop=False))
+
+    # Распознавание нейросетью
+    # И разбор результатов на индексы классов, оценки классов и ограничивающие рамки
+    for out in coco_net.forward(output_layers):
         for obj in out:
             scores = obj[5:]
             class_index = np.argmax(scores)
             class_score = scores[class_index]
             if class_score > 0:
-                center_x = int(obj[0] * width)
-                center_y = int(obj[1] * height)
-                obj_width = int(obj[2] * width)
-                obj_height = int(obj[3] * height)
-                box = [center_x - obj_width // 2, center_y - obj_height // 2, obj_width, obj_height]
-                boxes.append(box)
+                boxes.append(
+                    [
+                        int(obj[0] * width) - int(obj[2] * width) // 2,
+                        int(obj[1] * height) - int(obj[3] * height) // 2,
+                        int(obj[2] * width),
+                        int(obj[3] * height),
+                    ]
+                )
                 class_indexes.append(class_index)
                 class_scores.append(float(class_score))
 
-    # Keep boxes nms_threshold > 0.4
-    nms_boxes = cv2.dnn.NMSBoxes(boxes, class_scores, 0.0, 0.4)
+    # Возвращение локализующих прямоугольников, индексов распознанных классов и вероятностей
+    return boxes, class_indexes, class_scores
 
-    # Draw annotation box and text for each class
-    for box_index in list(nms_boxes):
-        # Extract class index
+
+def draw(frame, boxes, class_indexes, class_scores, classes):
+    # Нарисовать аннотацию и текст для каждого класса c охранённых ограничивающих рамок с порогом nms_threshold > 0.4
+    for box_index in list(cv2.dnn.NMSBoxes(boxes, class_scores, 0.0, 0.4)):
+        # Извлечение индекса класса
         class_index = class_indexes[box_index]
 
-        # Uncomment code below to exclude aeroplane from annotations
-        # if classes[class_index] == "aeroplane":
+        # Раскомментируйте код ниже, чтобы исключить книги из аннотаций (конспирация)
+        # TODO: УДАЛИТЬ ЭТОТ КОД ПЕРЕД ВСТАВКОЙ В ОТЧЁТ
+        # if classes[class_index] == "book":
         #    continue
 
-        # Extract bounding box
+        # Рамочка и текст класса
         x, y, w, h = boxes[box_index]
-
-        # Draw ROI
-        image_to_process = cv2.rectangle(image_to_process, (x, y), (x + w, y + h), colors[class_index], 1)
-
-        # Text
-        roi_text = f"{classes[class_index]}: {(class_scores[box_index] * 100):.0f}%"
-
-        # Draw text background
-        image_to_process = cv2.rectangle(
-            image_to_process,
-            (x, y - 20),
-            (x + len(roi_text) * 10, y),
-            colors[class_index],
-            -1,
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (69, 33, 0), 2)
+        frame = cv2.putText(
+            frame, classes[class_index].upper(), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (69, 33, 0), 2
         )
 
-        # Draw annotation text
-        image_to_process = cv2.putText(
-            image_to_process,
-            roi_text,
-            (x + 5, y - 5),
-            cv2.FONT_HERSHEY_PLAIN,
-            1,
-            (255 - colors[class_index][0], 255 - colors[class_index][1], 255 - colors[class_index][2]),
-            1,
-            cv2.LINE_AA,
-        )
-
-    return image_to_process
+    return frame
 
 
-def main() -> None:
-    """Main program entry"""
-    # Initialize logging
-    logging_setup()
-
-    # Load YOLO class names
-    logging.info(f"Loading class names from {YOLO_CLASSES_FILE}")
-    with open(os.path.join(YOLO_DIR, YOLO_CLASSES_FILE), "r", encoding="utf-8") as file:
+def main():
+    # Загрузка классов
+    with open(os.path.join("yolov4-tiny", "coco.names.txt"), "r", encoding="utf-8") as file:
         classes = file.read().split("\n")
-        classes = [class_name.replace("\r", "") for class_name in classes if len(class_name.replace("\r", "")) > 1]
 
-    # Create random color for each class
-    colors = []
-    for _ in range(len(classes)):
-        color = np.random.randint(0, 256, size=(3,))
-        colors.append((int(color[0]), int(color[1]), int(color[2])))
-
-    # Load YOLO weights and config
-    logging.info(f"Loading YOLO config from {YOLO_CFG_FILE} and weights from {YOLO_WEIGHTS_FILE}")
-    net = cv2.dnn.readNetFromDarknet(
-        os.path.join(YOLO_DIR, YOLO_CFG_FILE),
-        os.path.join(YOLO_DIR, YOLO_WEIGHTS_FILE),
+    # Загрузка модели из формата Darknet
+    coco_net = cv2.dnn.readNetFromDarknet(
+        os.path.join("yolov4-tiny", "yolov4-tiny.cfg"),
+        os.path.join("yolov4-tiny", "yolov4-tiny.weights"),
     )
-    layer_names = net.getLayerNames()
-    out_layers_indexes = net.getUnconnectedOutLayers()
-    out_layers = [layer_names[index - 1] for index in out_layers_indexes]
+    layer_names = coco_net.getLayerNames()
+    out_layers_indexes = coco_net.getUnconnectedOutLayers()
+    output_layers = [layer_names[index - 1] for index in out_layers_indexes]
 
-    # Start OpenCV video capture
-    capture = cv2.VideoCapture(PHOTO_VIDEO_INPUT)
-
-    # Main loop
+    # Запуск стрима OpenCV
+    video_capture = cv2.VideoCapture(TO_DETECT)
     while True:
-        # Capture the video frame by frame
-        _, frame = capture.read()
+        # Чтение кадра
+        ret, frame = video_capture.read()
 
-        # Exit if no frame
-        if frame is None:
-            # Wait for any key and exit
-            logging.info("Done! Press any key to exit")
+        # Выход при ошибке или если больше нет кадров
+        if not ret or frame is None:
             cv2.waitKey(0)
             break
 
-        # Detect, localize and annotate
-        frame = yolo_detect_and_localize(frame, net, out_layers, classes, colors)
+        # Распознавание и локализация
+        boxes, class_indexes, class_scores = detect(frame, coco_net, output_layers)
 
-        # Display the resulting frame
-        cv2.imshow("frame", frame)
+        # Отрисовка
+        frame = draw(frame, boxes, class_indexes, class_scores, classes)
+        cv2.imshow("Prakticheskaya rabota 2", frame)
 
-        # 30 - 1/30 FPS
-        # Press q to quit
+        # Нажмите q для выхода
         if cv2.waitKey(30) & 0xFF == ord("q"):
             break
 
-    # Release the cap object after the loop
-    if capture is not None:
-        capture.release()
-
-    # Destroy all OpenCV windows
+    # Остановка стрима и закрытие окна OpenCV
+    video_capture.release()
     cv2.destroyAllWindows()
 
 
