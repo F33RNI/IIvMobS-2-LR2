6dcdf29780a0744e371ac466ebc755a21df0928c
F33RNI <xxoinvizionxx@gmail.com>
2024-02-13 23:48:43 -0500

    feat: edit code and docs in this branch

--- main.py
+++ main.py
@@ -25,85 +25,14 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <https://unlicense.org>
 """
 
-import os
-
 import cv2
-import numpy as np
 
-# Видеофайл / изображение или индекс вебки (TO_DETECT = 0)
-TO_DETECT = "photos/books_and_apples.jpg"
-
-
-def detect(frame, coco_net, output_layers):
-    # Размер входной картинки
-    height, width, _ = frame.shape
-
-    # Подготовка списков для сохранения распознанных объектов
-    class_indexes, class_scores, boxes = ([] for _ in range(3))
-
-    # Вход нейросети
-    coco_net.setInput(cv2.dnn.blobFromImage(frame, 1 / 255, (608, 608), (0, 0, 0), swapRB=True, crop=False))
-
-    # Распознавание нейросетью
-    # И разбор результатов на индексы классов, оценки классов и ограничивающие рамки
-    for out in coco_net.forward(output_layers):
-        for obj in out:
-            scores = obj[5:]
-            class_index = np.argmax(scores)
-            class_score = scores[class_index]
-            if class_score > 0:
-                boxes.append(
-                    [
-                        int(obj[0] * width) - int(obj[2] * width) // 2,
-                        int(obj[1] * height) - int(obj[3] * height) // 2,
-                        int(obj[2] * width),
-                        int(obj[3] * height),
-                    ]
-                )
-                class_indexes.append(class_index)
-                class_scores.append(float(class_score))
-
-    # Возвращение локализующих прямоугольников, индексов распознанных классов и вероятностей
-    return boxes, class_indexes, class_scores
-
-
-def draw(frame, boxes, class_indexes, class_scores, classes):
-    # Нарисовать аннотацию и текст для каждого класса c охранённых ограничивающих рамок с порогом nms_threshold > 0.4
-    for box_index in list(cv2.dnn.NMSBoxes(boxes, class_scores, 0.0, 0.4)):
-        # Извлечение индекса класса
-        class_index = class_indexes[box_index]
-
-        # Раскомментируйте код ниже, чтобы исключить книги из аннотаций (конспирация)
-        # TODO: УДАЛИТЬ ЭТОТ КОД ПЕРЕД ВСТАВКОЙ В ОТЧЁТ
-        # if classes[class_index] == "book":
-        #    continue
-
-        # Рамочка и текст класса
-        x, y, w, h = boxes[box_index]
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (69, 33, 0), 2)
-        frame = cv2.putText(
-            frame, classes[class_index].upper(), (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (69, 33, 0), 2
-        )
-
-    return frame
+from detection import apply_yolo_object_detection
 
 
 def main():
-    # Загрузка классов
-    with open(os.path.join("yolov4-tiny", "coco.names.txt"), "r", encoding="utf-8") as file:
-        classes = file.read().split("\n")
-
-    # Загрузка модели из формата Darknet
-    coco_net = cv2.dnn.readNetFromDarknet(
-        os.path.join("yolov4-tiny", "yolov4-tiny.cfg"),
-        os.path.join("yolov4-tiny", "yolov4-tiny.weights"),
-    )
-    layer_names = coco_net.getLayerNames()
-    out_layers_indexes = coco_net.getUnconnectedOutLayers()
-    output_layers = [layer_names[index - 1] for index in out_layers_indexes]
-
-    # Запуск стрима OpenCV
-    video_capture = cv2.VideoCapture(TO_DETECT)
+    # Изображение / видеофайл / веб-стрим / изображение или индекс вебки (TO_DETECT = 0)
+    video_capture = cv2.VideoCapture("photos/persons_cars_and_bear.jpg")
     while True:
         # Чтение кадра
         ret, frame = video_capture.read()
@@ -114,17 +43,15 @@ def main():
             break
 
         # Распознавание и локализация
-        boxes, class_indexes, class_scores = detect(frame, coco_net, output_layers)
+        apply_yolo_object_detection(frame)
 
         # Отрисовка
-        frame = draw(frame, boxes, class_indexes, class_scores, classes)
-        cv2.imshow("Prakticheskaya rabota 2", frame)
+        cv2.imshow("LR2 jundevchik", frame)
 
-        # Нажмите q для выхода
+        # Ожидание q для выхода
         if cv2.waitKey(30) & 0xFF == ord("q"):
             break
 
-    # Остановка стрима и закрытие окна OpenCV
     video_capture.release()
     cv2.destroyAllWindows()
 
