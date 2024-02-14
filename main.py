74c6d47697782a83c9e56e8f0868fe977a43cc82
F33RNI <xxoinvizionxx@gmail.com>
2024-02-14 16:43:30 -0500

    feat: edit code for this branch

--- main.py
+++ main.py
@@ -25,36 +25,122 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <https://unlicense.org>
 """
 
+import os
+
 import cv2
+import numpy as np
 
-from detection import apply_yolo_object_detection
+# Загрузка модели
+print("Загрузка модели...")
+net = cv2.dnn.readNetFromDarknet(
+    os.path.join("Resources", "yolov4-tiny.cfg"), os.path.join("Resources", "yolov4-tiny.weights")
+)
+layer_names = net.getLayerNames()
+out_layers_indexes = net.getUnconnectedOutLayers()
+out_layers = [layer_names[index - 1] for index in out_layers_indexes]
+print("Модель загружена!")
 
-
-def main():
-    # Изображение / видеофайл / веб-стрим / изображение или индекс вебки (TO_DETECT = 0)
-    video_capture = cv2.VideoCapture("photos/persons_cars_and_bear.jpg")
-    while True:
-        # Чтение кадра
-        ret, frame = video_capture.read()
-
-        # Выход при ошибке или если больше нет кадров
-        if not ret or frame is None:
-            cv2.waitKey(0)
-            break
-
-        # Распознавание и локализация
-        apply_yolo_object_detection(frame)
-
-        # Отрисовка
-        cv2.imshow("LR2 jundevchik", frame)
-
-        # Ожидание q для выхода
-        if cv2.waitKey(30) & 0xFF == ord("q"):
-            break
-
-    video_capture.release()
-    cv2.destroyAllWindows()
+# Загрузка названий классов
+print("Загрузка классов...")
+with open(os.path.join("Resources", "coco.names.txt"), "r", encoding="utf-8") as file:
+    classes = file.read().split("\n")
+print("Классы загружены!")
 
 
-if __name__ == "__main__":
-    main()
+# Изображение / видеофайл / веб-стрим / изображение или индекс вебки (TO_DETECT = 0)
+print("Запуск распознавания...")
+cap = cv2.VideoCapture("photos/elephant_and_person.jpg")
+while True:
+    # Чтение кадра
+    ret, frame = cap.read()
+
+    # Выход при ошибке или если больше нет кадров
+    if not ret or frame is None:
+        print("Распознавание окончено! Нажмите любую клавишу для выхода")
+        cv2.waitKey(0)
+        break
+
+    # Размер кадра
+    height, width, _ = frame.shape
+
+    # Распознавание
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255, (608, 608), (0, 0, 0), swapRB=True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(out_layers)
+
+    # Временные переменные
+    class_indexes, class_scores, boxes = ([] for i in range(3))
+    objects_count = 0
+    objects_found_names = []
+
+    # Разбор обнаруженных объектов
+    for out in outs:
+        for obj in out:
+            scores = obj[5:]
+            index = np.argmax(scores)
+            class_score = scores[index]
+            if class_score > 0:
+                center_x = int(obj[0] * width)
+                center_y = int(obj[1] * height)
+                obj_width = int(obj[2] * width)
+                obj_height = int(obj[3] * height)
+                box = [center_x - obj_width // 2, center_y - obj_height // 2, obj_width, obj_height]
+                boxes.append(box)
+                class_indexes.append(index)
+                class_scores.append(float(class_score))
+
+    # Отбор
+    chosen_boxes = cv2.dnn.NMSBoxes(boxes, class_scores, 0.0, 0.4)
+    for box_index in chosen_boxes:
+        box = boxes[box_index]
+        index = class_indexes[box_index]
+
+        # Раскомментируйте код ниже, чтобы исключить слона из аннотаций (конспирация)
+        # TODO: УДАЛИТЬ ЭТОТ КОД ПЕРЕД ВСТАВКОЙ В ОТЧЁТ
+        # if classes[index] == "elephant":
+        #    continue
+
+        # Обновление количества и списка обнаруженных объектов
+        objects_count += 1
+        objects_found_names.append(classes[index])
+
+        # Координаты обнаруженного объекта
+        x, y, w, h = box
+        start = (x, y)
+        end = (x + w, y + h)
+
+        # Рамочка
+        frame = cv2.rectangle(frame, start, end, (255, 255, 255), 2)
+
+        # Текст
+        frame = cv2.putText(
+            frame, classes[index], (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, cv2.LINE_AA
+        )
+
+    # Вывод количества обнаруженных объектов в терминал
+    print(f"Обнаружено: {objects_count} объектов. Нажмите 'q' для выхода")
+
+    # Вывод количества обнаруженных объектов на кадр
+    frame = cv2.putText(
+        frame,
+        f"Found {objects_count} objects: {', '.join(objects_found_names)}",
+        (10, 30),
+        cv2.FONT_HERSHEY_PLAIN,
+        1,
+        (255, 255, 255),
+        1,
+        cv2.LINE_AA,
+    )
+
+    # Отрисовка кадра
+    cv2.imshow("LR2 drmoonshine", frame)
+
+    # Ожидание q для выхода
+    if cv2.waitKey(30) & 0xFF == ord("q"):
+        break
+
+# Закрытие файла / остановки стрима
+cap.release()
+
+# Закрытие всех окон
+cv2.destroyAllWindows()
